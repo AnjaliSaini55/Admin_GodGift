c4e095e1e8127fb019c36f27e000df02d35725c7
AnjaliSaini55 <119155985+AnjaliSaini55@users.noreply.github.com>
2023-02-24 12:53:57 +0530

    Update Products.jsx

--- src/Admin/Pages/Products.jsx
+++ src/Admin/Pages/Products.jsx
@@ -1,22 +1,17 @@
 import React, { useEffect, useState } from "react";
-import Accordion from "react-bootstrap/Accordion";
 import "./pages.css";
 import MainLayout from "./MainLayout";
 import { useNavigate, useParams } from "react-router-dom";
 import { collection, deleteDoc, doc, onSnapshot } from "firebase/firestore";
 import { db } from "../../firebase";
 import { Button, Container, Table, Col, Row } from "react-bootstrap";
-import ModalComp from "./ModalComp";
-import ProductForm from "./ProductForm";
-import { BsPlus } from "react-icons/bs";
+import { BsPlusLg } from "react-icons/bs";
+import Swal from "sweetalert2";
 
 const Products = () => {
   const [users, setUsers] = useState([]);
-  const [open, setOpen] = useState(false);
-  const [user, setUser] = useState({});
   const [loading, setLoading] = useState(false);
   const navigate = useNavigate();
-  const { id } = useParams();
 
   useEffect(() => {
     setLoading(true);
@@ -38,93 +33,94 @@ const Products = () => {
       unsub();
     };
   }, []);
-  const handleDelete = async (id) => {
-    if (window.confirm("Are you sure to delete that user?")) {
-      try {
-        await deleteDoc(doc(db, "users", id));
-        setUsers(users.filter((user) => user.id !== id));
-      } catch (err) {
-        console.log(err);
-      }
-    }
 
-    navigate("/");
+  const deleteDatas = async (id) => {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        deleteDoc(doc(db, `users/${id}`));
+        Swal.fire("Deleted!", "Your file has been deleted.", "success");
+      }
+    });
   };
-  const handleModal = (item) => {
-    setOpen(true);
-    setUser(item);
+  const handleAddData = () => {
+    navigate("/product-form");
   };
   console.log(users);
   return (
     <>
       <MainLayout />
+        <Container>
+          <Row>
+            <Col style={{ padding: "30px", textAlign: "left" }} sm={6}>
+              <h4>PRODUCTS</h4>
+            </Col>
+            <Col style={{ padding: "30px", textAlign: "right" }} sm={6}>
+              <Button
+                variant="dark"
+                style={{ margin: "auto" }}
+                onClick={() => handleAddData()}
+              >
+                <BsPlusLg />
+              </Button>
+            </Col>
+            <hr />
+          </Row>
+        </Container>
       <div className="main-div">
-        <Accordion defaultActiveKey={["0"]} alwaysOpen>
-        <Accordion.Item eventKey="1">
-            <Accordion.Header><BsPlus /></Accordion.Header>
-            <Accordion.Body>
-              <ProductForm />
-            </Accordion.Body>
-          </Accordion.Item>
-          <Accordion.Item eventKey="0">
-            <Accordion.Header><h4>Products</h4></Accordion.Header>
-            <Accordion.Body>
-              <Container>
-                  {users &&
-                    users.map((item) => (
-                      <Row key={item.id}>
-                        <Table responsive="sm">
-                          <tbody>
-                            <tr>
-                              <td>
-                                <div
-                                  style={{ display: "flex", margin: "auto" }}
-                                >
-                                  <img
-                                    src={item.img}
-                                    style={{
-                                      height: "60px",
-                                      width: "60px",
-                                      margin: "auto",
-                                      borderRadius: "50%",
-                                    }}
-                                  />
-                                  <p style={{ margin: "auto",width: "150px" }}>{item.name}</p>
-                                  <p style={{ margin: "auto",width: "150px" }}>{item.info}</p>
-                                  <Button
-                                    variant="success"
-                                    style={{ margin: "auto" }}
-                                    onClick={() =>
-                                      navigate(`/product-form/${item.id}`)
-                                    }
-                                  >
-                                    Update
-                                  </Button>&nbsp;&nbsp;
-                                  <Button variant="dark" style={{ margin:"auto"}}
-                              onClick={() => handleModal(item)}
-                            >
-                              View
-                            </Button>
-                                  <ModalComp style={{ margin: "auto" }} />
-                                  {open && (
-                                    <ModalComp
-                                      open={open}
-                                      setOpen={setOpen}
-                                      handleDelete={handleDelete}
-                                      {...user}
-                                    />
-                                  )}
-                                </div>
-                              </td>
-                            </tr>
-                          </tbody>
-                        </Table>
-                      </Row>
-                    ))}
-              </Container>
-            </Accordion.Body>
-          </Accordion.Item>
-        </Accordion>
+        <Container>
+          {users &&
+            users.map((item) => (
+              <Row key={item.id}>
+                <Table responsive="sm">
+                  <tbody>
+                    <tr>
+                      <td>
+                        <div style={{ display: "flex", margin: "auto" }}>
+                          <img
+                            src={item.img}
+                            style={{
+                              height: "60px",
+                              width: "60px",
+                              margin: "auto",
+                              borderRadius: "50%",
+                            }}
+                          />
+                          <p style={{ margin: "auto", width: "150px" }}>
+                            {item.name}
+                          </p>
+                          <p style={{ margin: "auto", width: "150px" }}>
+                            {item.info}
+                          </p>
+                          <Button
+                            variant="success"
+                            style={{ margin: "auto" }}
+                            onClick={() => navigate(`/product-form/${item.id}`)}
+                          >
+                            Update
+                          </Button>
+                          <Button
+                            variant="danger"
+                            style={{ margin: "auto" }}
+                            onClick={() => deleteDatas(item.id)}
+                          >
+                            Delete
+                          </Button>
+                        </div>
+                      </td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </Row>
+            ))}
+        </Container>
       </div>
     </>
   );
