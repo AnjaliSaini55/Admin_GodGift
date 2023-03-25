0b79c03d14c0222338903600e912162b4779a40f
AnjaliSaini55 <119155985+AnjaliSaini55@users.noreply.github.com>
2023-03-25 12:37:40 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -6,14 +6,7 @@ import 'firebase/compat/firestore';
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBU7-KUzzkow45VNRBqu_zr_3dWImEs6s4",
-  authDomain: "god-gift-login.firebaseapp.com",
-  databaseURL: "https://god-gift-login-default-rtdb.firebaseio.com",
-  projectId: "god-gift-login",
-  storageBucket: "god-gift-login.appspot.com",
-  messagingSenderId: "113288393496",
-  appId: "1:113288393496:web:9d3e27ac20bc06e67be46d",
-  measurementId: "G-L5VW1BM02G"
+//   your firebase configration
 };
 
 // Initialize Firebase and Firebase Authentication
